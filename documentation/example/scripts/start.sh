ec7e0fa5c4e07d03cf696ae81ba1fea005e65cbe
begenFmg <n_perminov@marlerino.group>
2024-03-04 19:34:24 +0500

    docs: complete work docker

--- documentation/example/scripts/start.sh
+++ documentation/example/scripts/start.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-alembic upgrade head
-
-gunicorn core.server:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
+gunicorn core.server:app --workers $WORKERS --worker-class uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
